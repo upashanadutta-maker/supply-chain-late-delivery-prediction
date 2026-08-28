09b86b130b274ea0928cf59c92010f57ba17467a
Upashana Dutta <112981832+upashanadutta-maker@users.noreply.github.com>
2026-08-28 16:46:19 +0530

    Create test_preprocessing.py

---
+++ tests/test_preprocessing.py
@@ -0,0 +1,79 @@
+import pandas as pd
+
+from src.preprocessing import (
+    fit_preprocessor,
+    transform_features,
+)
+
+
+def test_preprocessing_pipeline():
+
+    train_data = pd.DataFrame({
+        "Type": ["DEBIT", "CASH", "DEBIT"],
+        "Shipping Mode": [
+            "Standard Class",
+            "First Class",
+            "Standard Class",
+        ],
+        "order date (DateOrders)": [
+            "1/1/2018 10:00",
+            "1/2/2018 15:00",
+            "1/3/2018 20:00",
+        ],
+        "Delivery Status": [
+            "Late delivery",
+            "Advance shipping",
+            "Late delivery",
+        ],
+        "Order Id": [1, 2, 3],
+        "Product Status": [0, 0, 0],
+    })
+
+    processed_train, artifacts = fit_preprocessor(
+        train_data
+    )
+
+    assert "Delivery Status" not in processed_train.columns
+    assert "Order Id" not in processed_train.columns
+    assert "order date (DateOrders)" not in processed_train.columns
+
+    assert "order_month" in processed_train.columns
+    assert "order_dayofweek" in processed_train.columns
+    assert "order_hour" in processed_train.columns
+
+    assert "Product Status" not in processed_train.columns
+
+
+def test_unseen_category():
+
+    train_data = pd.DataFrame({
+        "Type": ["DEBIT", "CASH"],
+        "Shipping Mode": [
+            "Standard Class",
+            "First Class",
+        ],
+        "order date (DateOrders)": [
+            "1/1/2018 10:00",
+            "1/2/2018 15:00",
+        ],
+    })
+
+    new_data = pd.DataFrame({
+        "Type": ["TRANSFER"],
+        "Shipping Mode": ["Same Day"],
+        "order date (DateOrders)": [
+            "1/3/2018 20:00"
+        ],
+    })
+
+    _, artifacts = fit_preprocessor(
+        train_data
+    )
+
+    processed_new = transform_features(
+        new_data,
+        artifacts
+    )
+
+    assert processed_new["Type"].iloc[0] == 0
+    assert processed_new["Shipping Mode"].iloc[0] == 0

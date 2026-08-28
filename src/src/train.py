71ff8338054e92b5ad959fc869183785c69b0208
Upashana Dutta <112981832+upashanadutta-maker@users.noreply.github.com>
2026-08-28 16:16:16 +0530

    Update train.py

--- src/src/train.py
+++ src/src/train.py
@@ -1,1 +1,18 @@
 
+import pandas as pd
+
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.metrics import roc_auc_score
+
+from src.preprocessing import (
+    fit_preprocessor,
+    transform_features,
+)
+
+
+def main():
+    print("Training script started.")
+
+
+if __name__ == "__main__":
+    main()
